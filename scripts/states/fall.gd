3c1b870aaab3afa3b355d0f433f7bdbb47b61a23
Bryan <bryan87reed@gmail.com>
2023-01-14 13:59:51 -0700

    improve jump

--- scripts/states/fall.gd
+++ scripts/states/fall.gd
@@ -3,19 +3,33 @@ extends BaseState
 export (NodePath) var run_node
 export (NodePath) var idle_node
 export (NodePath) var dash_node
+export (NodePath) var jump_node
 
 onready var run_state: BaseState = get_node(run_node)
 onready var idle_state: BaseState = get_node(idle_node)
 onready var dash_state: BaseState = get_node(dash_node)
+onready var jump_state: BaseState = get_node(jump_node)
 
 export (int) var max_speed = 150
 export (int) var max_fall_speed= 500
 export (int) var accel = 40
+export (float) var jump_buffer_time = 0.1
 
+var jump_buffer_timer: float = 0
+
+func enter() -> void:
+	.enter()
+	jump_buffer_timer = 0
 
 func input(event: InputEvent) -> BaseState:
 	if player.can_dash and Input.is_action_just_pressed('dash_kb'):
 		return dash_state
+	if Input.is_action_just_pressed('jump_kb'):
+		jump_buffer_timer = jump_buffer_time
+	return null
+	
+func process(delta: float) -> BaseState:
+	jump_buffer_timer -= delta
 	return null
 
 func physics_process(delta: float) -> BaseState:
@@ -36,6 +50,8 @@ func physics_process(delta: float) -> BaseState:
 	player.velocity = player.move_and_slide(player.velocity, Vector2.UP)
 
 	if player.is_on_floor():
+		if jump_buffer_timer > 0:
+			return jump_state
 		if player.moving != 0:
 			return run_state
 		else:
