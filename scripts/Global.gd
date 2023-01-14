63f824159849c521ecbc7ea14181b0bf955a6de9
Ross <ross@eml.cc>
2023-01-13 18:25:35 -0800

    WIP controller mapping

---
+++ scripts/Global.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+var player_input_devices = {
+	'p1': 'keyboard',
+	'p2': null,
+	'p3': null,
+	'p4': null,
+}
+
+# physical input duplicate entries
+var ghost_inputs = ["Steam Virtual Gamepad"]
