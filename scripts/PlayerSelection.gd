3d78ab1a3804aa8065c8edd3accac133013db136
Ross <ross@eml.cc>
2023-01-14 19:34:27 -0800

    player select ready up

--- scripts/PlayerSelection.gd
+++ scripts/PlayerSelection.gd
@@ -1,7 +1,13 @@
 extends Control
+# warning-ignore-all:return_value_discarded
+
+var player_ready_status: Dictionary = {}
+var aye_aye_captain = false
+var ready_text = ["3", "2", "1", "GO!"]
+var ready_index = null
 
 func _ready():
-	print("adding player p1")
+	$Camera2D.current = true
 	$Boxes/Box1.add_player("p1")
 
 func _input(event):
@@ -31,3 +37,75 @@ func handle_new_input(input_device):
 			g.player_input_devices[player_key] = input_device
 			get_node("Boxes/Box" + str(i + 1)).add_player(player_key)
 			break
+
+func handle_player_joined(player_key):
+	if not player_key:
+		return
+	player_ready_status[player_key] = false
+	are_you_ready_kids()
+
+func handle_player_left(player_key):
+	if not player_key:
+		return
+	if player_ready_status.has(player_key):
+		player_ready_status.erase(player_key)
+		are_you_ready_kids()
+
+func handle_player_ready_changed(player_key, ready_status):
+	if not player_key:
+		return
+	player_ready_status[player_key] = ready_status
+	are_you_ready_kids()
+
+func are_you_ready_kids():
+	aye_aye_captain = true
+	ready_index = null
+	for p in player_ready_status:
+		if not player_ready_status[p]:
+			aye_aye_captain = false
+	if aye_aye_captain:
+		begin_ready_countdown()
+	else:
+		cancel_ready_countdown()
+
+func begin_ready_countdown():
+	ready_index = 0
+	show_ready_text()
+
+func show_ready_text():
+	if ready_index == null:
+		cancel_ready_countdown()
+		return
+	$ReadyCountdown.text = ready_text[ready_index]
+	$ReadyCountdown.modulate.a = 1.0
+	$ReadyCountdown/GrowTween.interpolate_property($ReadyCountdown, "rect_scale", Vector2(4, 4), Vector2(5, 5), 0.75, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$ReadyCountdown/GrowTween.start()
+
+func cancel_ready_countdown():
+	ready_index = null
+	$ReadyCountdown.text = ""
+	$ReadyCountdown.modulate.a = 1.0
+	$ReadyCountdown/GrowTween.stop_all()
+	$ReadyCountdown/FadeTween.stop_all()
+
+# grow the text first
+func _on_GrowTween_tween_all_completed():
+	if not aye_aye_captain:
+		cancel_ready_countdown()
+		return
+	$ReadyCountdown/FadeTween.interpolate_property($ReadyCountdown, "rect_scale", Vector2(5, 5), Vector2(4, 4), 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$ReadyCountdown/FadeTween.interpolate_property($ReadyCountdown, "modulate:a", 1.0, 0.0, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$ReadyCountdown/FadeTween.start()
+
+# then fade and shrink text
+func _on_FadeTween_tween_all_completed():
+	if not aye_aye_captain or ready_index == null:
+		cancel_ready_countdown()
+		return
+	if ready_index < ready_text.size() - 1:
+		ready_index += 1
+		show_ready_text()
+	else:
+		## TODO: start game
+		print("START GAME")
+		return
--- scripts/PlayerSelectionBox.gd
+++ scripts/PlayerSelectionBox.gd
@@ -1,24 +1,39 @@
 extends Control
+# warning-ignore-all:return_value_discarded
 
-var join_high_pos = 90
-var join_mid_pos = 100
-var join_low_pos = 110
+signal player_joined(player_key)
+signal player_left(player_key)
+signal player_ready_changed(player_key, ready_status)
 
 var keyboard = preload("res://assets/keyboard.png")
 var controller = preload("res://assets/controller.png")
 
+onready var player_selection = get_parent().get_parent()
 ## TODO: custom button styleboxs
 onready var focused_stylebox = $Model/Prev.get_stylebox("focus").duplicate()
 onready var pressed_stylebox = $Model/Prev.get_stylebox("pressed").duplicate()
 onready var buttons = [[$Model/Prev, $Model/Next], [$Color/Prev, $Color/Next], [$Leave, $Ready]]
+
 var focused_button_index = null
 var pressed_button = null
+var player_key = null
+var is_ready = null
+var join_high_pos = 90
+var join_mid_pos = 100
+var join_low_pos = 110
 
 func _ready():
+	connect("player_joined", player_selection, "handle_player_joined")
+	connect("player_left", player_selection, "handle_player_left")
+	connect("player_ready_changed", player_selection, "handle_player_ready_changed")
 	$Player.ui_disabled = true
+	$Player/Camera2D.current = false
 	remove_player()
 
 func remove_player():
+	emit_signal("player_left", player_key)
+	player_key = null
+	$Name.text = ""
 	$Player.visible = false
 	$InputDevice.visible = false
 	$Model.visible = false
@@ -33,7 +48,9 @@ func remove_player():
 	$Join/Tween.interpolate_property($Join, "rect_position:y", $Join.rect_position.y, join_high_pos, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	$Join/Tween.start()
 
-func add_player(player_key):
+func add_player(p_key):
+	emit_signal("player_joined", p_key)
+	player_key = p_key
 	$Name.text = player_key
 	$Join.visible = false
 	
@@ -68,8 +85,8 @@ func disable_cursor():
 
 func _input(event):
 	if event.is_action_pressed('any_pad_interaction') \
-		and $Name.text in g.player_input_devices.keys() \
-		and g.player_input_devices[$Name.text] == "joy_" + str(event.device):
+		and player_key in g.player_input_devices.keys() \
+		and g.player_input_devices[player_key] == "joy_" + str(event.device):
 			var device_index = str(event.device)
 			if event.is_action_pressed("ui_up_" + device_index):
 				focus_prev_row()
@@ -138,10 +155,43 @@ func _on_Tween_tween_all_completed():
 
 # hide cursor if hovering over controller box, otherwise visible
 func _on_Box_mouse_entered():
-	if $Name.text in g.player_input_devices.keys():
-		if g.player_input_devices[$Name.text] == "keyboard":
+	if player_key in g.player_input_devices.keys():
+		if g.player_input_devices[player_key] == "keyboard":
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _on_ModelPrev_pressed():
+	# TODO
+	pass
+
+func _on_ModelNext_pressed():
+	# TODO
+	pass
+
+func _on_ColorPrev_pressed():
+	# TODO
+	pass
+
+func _on_ColorNext_pressed():
+	# TODO
+	pass
+
+func _on_Leave_pressed():
+	# TODO
+	pass
+
+func _on_Ready_pressed():
+	is_ready = !is_ready
+	$Checkmark.visible = is_ready
+	$Ready.text = "UNREADY" if is_ready else "READY"
+	$Ready.rect_size.x = 73
+	emit_signal("player_ready_changed", player_key, is_ready)
+	if is_ready:
+		# TODO: change button to red
+		return
+	else:
+		# TODO: change button to green
+		return
