06c89ece0b3de9debfc9ebcde49eb9148d65b0c1
Bryan <bryan87reed@gmail.com>
2023-01-20 14:50:16 -0700

    jump pads can be disabled

--- scripts/JumpPad.gd
+++ scripts/JumpPad.gd
@@ -1,16 +1,24 @@
 extends Node2D
 
 export var jump_force_multiplier: float = 2.0
+export var disabled = false
 
 func _ready():
-	$AnimatedSprite.playing = true
+	if not disabled:
+		$AnimatedSprite.playing = true
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("players") and not body.is_dead:
-		body.jump_force_multiplier = jump_force_multiplier
-		body.jump_padding = true
+		if disabled:
+			$Label.visible = true
+		else:
+			body.jump_force_multiplier = jump_force_multiplier
+			body.jump_padding = true
 
 
 func _on_Area2D_body_exited(body):
 	if body.is_in_group("players") and not body.is_dead:
-		body.jump_padding = false
+		if disabled:
+			$Label.visible = false
+		else:
+			body.jump_padding = false
