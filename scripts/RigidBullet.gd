270bede4a884f26488e5f84d9548bb7a50c7447f
Bryan <bryan87reed@gmail.com>
2023-01-20 13:47:17 -0700

    mortar explody behavior much better

--- scripts/RigidBullet.gd
+++ scripts/RigidBullet.gd
@@ -3,6 +3,9 @@ extends RigidBody2D
 var direction = 1
 var damage = 2
 var speed = 420
+var exploded = false
+onready var explody_floor_frames = preload("res://resources/floor_explody.tres") 
+onready var explody_air_frames = preload("res://resources/air_explody.tres") 
 
 func _ready():
 	var x = speed * direction
@@ -10,6 +13,8 @@ func _ready():
 	$AnimatedSprite.play()
 
 func _on_Area2D_body_entered(body):
+	if exploded:
+		return
 	if body.is_in_group('players') and body.has_method('dmg') and body.get("is_dead") == false:
 		body.dmg(damage)
 		show_and_play_explody()
@@ -17,9 +22,13 @@ func _on_Area2D_body_entered(body):
 		# TODO if reflective, bounce bullet back
 		show_and_play_explody()
 	elif not body.has_method("dmg"): # aka if body is a wall
+		if $RayCast2D.is_colliding():
+			$Explody.frames = explody_floor_frames
 		show_and_play_explody()
 
 func show_and_play_explody():
+	sleeping = true
+	exploded = true
 	$AnimatedSprite.visible = false
 	$Explody.visible = true
 	$Explody.play()
