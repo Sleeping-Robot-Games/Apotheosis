d7d6aeb210c0e4924e0963a31a111268258eaae0
Bryan <bryan87reed@gmail.com>
2023-01-19 21:36:14 -0700

    small update

--- scripts/HealingConsole.gd
+++ scripts/HealingConsole.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
-
+var healing = false
 
 func _ready():
 	$Idle.play()
 
+func _on_Area2D_body_entered(body):
+	if body.is_in_group('players'):
+		pass # Start healing animation
+		# Disable player
+		# Send the global position to the healing console
+		# Once active animation finishes un-disable the player
