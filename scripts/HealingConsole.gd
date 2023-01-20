e22f96d11373f4eae3d9635119b600bc376608ae
Bryan <bryan87reed@gmail.com>
2023-01-19 23:45:27 -0700

    Healing and impact animation

--- scripts/HealingConsole.gd
+++ scripts/HealingConsole.gd
@@ -1,13 +1,36 @@
 extends Node2D
 
-var healing = false
+var is_healing = false
+var healing_power = 5
+var heal_receiver = null
 
 func _ready():
 	$Idle.play()
+	$Label.visible = false
+
+func _input(event):
+	if heal_receiver and Input.is_action_just_pressed("fab_"+ heal_receiver.controller_id):
+		is_healing = true
+		heal_receiver.ui_disabled = true
+		heal_receiver.global_position = global_position
+		$Idle.visible = false
+		$Active.visible = true
+		$Active.play()
 
 func _on_Area2D_body_entered(body):
-	if body.is_in_group('players'):
-		pass # Start healing animation
-		# Disable player
-		# Send the global position to the healing console
-		# Once active animation finishes un-disable the player
+	if body.is_in_group('players') and not heal_receiver:
+		heal_receiver = body
+		$Label.visible = true
+
+func _on_Area2D_body_exited(body):
+	if body.is_in_group('players') and not is_healing:
+		heal_receiver = null
+		$Label.visible = false
+
+func _on_Active_animation_finished():
+	$Active.visible = false
+	$Idle.visible = true
+	if heal_receiver:
+		g.player_ui[heal_receiver.player_key].set_health(max(heal_receiver.hp + healing_power, 10))
+		heal_receiver.ui_disabled = false
+	is_healing = false
--- scripts/RigidBullet.gd
+++ scripts/RigidBullet.gd
@@ -9,14 +9,20 @@ func _ready():
 	linear_velocity = Vector2(x, -400)
 	$AnimatedSprite.play()
 
-
 func _on_Area2D_body_entered(body):
 	if body.is_in_group('players') and body.has_method('dmg') and body.get("is_dead") == false:
 		body.dmg(damage)
-		queue_free()
+		show_and_play_explody()
 	elif body.is_in_group("shields"):
 		# TODO if reflective, bounce bullet back
-		queue_free()
+		show_and_play_explody()
 	elif not body.has_method("dmg"): # aka if body is a wall
-		queue_free()
+		show_and_play_explody()
 
+func show_and_play_explody():
+	$AnimatedSprite.visible = false
+	$Explody.visible = true
+	$Explody.play()
+
+func _on_Explody_animation_finished():
+	queue_free()
