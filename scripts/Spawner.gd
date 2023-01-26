f70c5319cbe8ddcf00b6b3e909c2740f9ea353e9
Ross <ross@eml.cc>
2023-01-25 16:40:18 -0800

    don't spawning enemies on top of players

--- scripts/Spawner.gd
+++ scripts/Spawner.gd
@@ -8,6 +8,7 @@ export var boi_count = 3
 
 var enemy_scene
 var interval
+var nearby_players = []
 
 func _ready():
 	random.randomize()
@@ -20,7 +21,9 @@ func _ready():
 		$Timer.start()
 
 func _on_Timer_timeout():
-	if tower_state == g.tower_state and get_tree().get_nodes_in_group(name).size() <= boi_count:
+	if tower_state == g.tower_state \
+		and get_tree().get_nodes_in_group(name).size() <= boi_count \
+		and nearby_players.size() == 0:
 		interval = interval - (g.total_kills * (interval - 2) / g.total_max_kills)
 		$Timer.wait_time = interval
 		var new_enemy = enemy_scene.instance()
@@ -28,3 +31,13 @@ func _on_Timer_timeout():
 		get_node('../../Enemies').call_deferred('add_child', new_enemy)
 		new_enemy.add_to_group(name)
 		new_enemy.add_to_group('tower_state_'+str(tower_state))
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group('players'):
+		nearby_players.append(body)
+
+
+func _on_Area2D_body_exited(body):
+	if body.is_in_group('players'):
+		nearby_players.erase(body)
